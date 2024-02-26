a547547a2cf85bb57d28c92296c812a64f2e764c
danielaniceto <danielgustavots@hotmail.com>
2024-02-26 14:20:54 -0300

    Atividades ate as 14:20

--- api.py
+++ api.py
@@ -7,17 +7,17 @@ from src.controllers.controller import *
 app.run(port=4000, host="localhost", debug= True)
 
 @app.route("public/index", methods=["GET"])
-def is_obter_produtos():
+def is_criar_produtos():
     return jsonify(IndexController.get)
 
 @app.route("/", methods=["POST"])
-def is_deletar_produtos():
+def is_delete_produtos():
     return jsonify(DeleteProdutosController.post)
 
 @app.route("public/update", methods=["POST"])
-def is_atualizar_produtos():
+def is_update_produtos():
     return jsonify(UpdateProdutosController.post)
 
 @app.route("public/read", methods=["GET"])
-def is_retornar_produtos():
+def is_read_produtos():
     return jsonify(ReadProdutosController.get)
--- src/database/db.sql
+++ src/database/db.sql
@@ -4,10 +4,10 @@ USE produtosDB;
 
 CREATE TABLE [IF NOT EXISTS] produtos (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    descricao VARCHAR(5000),
-    preco DECIMAL(10, 2) NOT NULL,
-    quantidade INT NOT NULL
+    nome VARCHAR(100) NOT NULL, 
+    descricao VARCHAR(5000), 
+    preco DECIMAL DOUBLE NOT NULL,
+    quantidade INT NOT NULL,
 );
 
 ALTER TABLE produtos ADD FOREIGN KEY(id_produto) REFERENCES produtos(id)
