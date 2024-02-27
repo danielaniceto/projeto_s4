6305da16a83e2e3c62af4fda959461feda612916
danielaniceto <danielgustavots@hotmail.com>
2024-02-27 10:39:01 -0300

    Adequação banco

--- src/database/db.sql
+++ src/database/db.sql
@@ -2,12 +2,9 @@ CREATE DATABASE produtosDB;
 
 USE produtosDB;
 
-CREATE TABLE [IF NOT EXISTS] produtos (
+CREATE TABLE IF NOT EXISTS produtos (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL, 
     descricao VARCHAR(5000), 
-    preco DECIMAL DOUBLE NOT NULL,
-    quantidade INT NOT NULL,
+    preco DOUBLE NOT NULL
 );
-
-ALTER TABLE produtos ADD FOREIGN KEY(id_produto) REFERENCES produtos(id)
